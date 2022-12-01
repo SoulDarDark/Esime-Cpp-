0b689ead46bece9e48159d709bd0dfcf92524965
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2022-12-01 14:25:22 -0600

    Create 8-. Gradiente.cpp

---
+++ 8-. Gradiente.cpp
@@ -0,0 +1,38 @@
+// Juego Matematicas.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// Programa que calcula el gradiente
+//Crear un programa en c++ que sirva para derivar unba función exponencial básica de datos almacenados en el programa, no olvide utilizar objetos.
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class grad {
+    int dx;
+public:
+    void g(int x, int y, int z, int n);
+};
+
+void grad::g(int x, int y, int z, int n) {
+    dx = x * y ^ n;
+
+    cout << "Ingrese el valor de x";
+    cin >> x;
+    cout << "Ingrese el valor de y";
+    cin >> y;
+    cout << "Ingrese el valor de n";
+    cin >> n;
+    cout << "Ingrese el valor de z";
+    cin >> z;
+
+    cout << "dx=" << dx;
+}
+   
+
+int main() {
+    grad uno; //declare dos variables de tipo grad 
+
+    uno.g(x,y,n,z); //falta que las funciones salgan
+    
+    return 0;
+
+}
