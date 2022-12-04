adf48c5201a6833876937354a79e6c50cd3bec39
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2022-12-04 15:17:18 -0600

    Update 8.1-. Gradiente Corregido.cpp

--- 8.1-. Gradiente Corregido.cpp
+++ 8.1-. Gradiente Corregido.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 class gradiente{
-    int z,y,n,a;
-    int px,py,nd; //parcial
+    int x,z,y,n;
+    int px,py,pz,nd; //parcial
     public:
     parcial();
     grad();
@@ -11,39 +11,48 @@ class gradiente{
 };
 
 gradiente::parcial(){
-    cout<<"El programa saca la derivada parcial \n\n";
+
+    cout<<" *1-. derivadas parciales * \n";
     cout<<"ingrese x:";
-    cin>>a;
+    cin>>x;
     cout<<"ingrese y:";
     cin>>y;
     cout<<"ingrese n:";
     cin>>n;
+    cout<<"ingrese z:";
+    cin>>z;
 
-    if(a=y=n=0){
-        px=0;
-        px=0;
-        cout<<"la derivada parcial dx:"<<px<<endl;
-        cout<<"la derivada parcial dy:"<<py<<endl;
-    }
-    if(a=y){
-        px=y;
-        py=a;
-        
-        cout<<"la derivada parcial dx:"<<px<<endl;
-        cout<<"la derivada parcial dy:"<<py<<endl;
-    }
-  
-    if(y==1)
+    /*-----1-----*/
+    if(x==0,y==0,n==0,z==0){ 
+
+        cout<<endl;
+        cout<<"la derivada parcial dx:"<<x<<endl;
+        cout<<"la derivada parcial dy:"<<y<<endl;
+        cout<<"la derivada parcial dz:"<<z<<endl;
+        cout<<endl;
+
+    /*-----2-----*/
+    }else if(y==1) 
     { //para no imprimir todo, tiene que imprimir ay^n
-        px= a;
+        px= x;
         py=px*y;
         nd=n-1;
         cout<<"\n";
         cout<<"\n la derivada parcial de x: "<<px<<"y^"<<nd<<endl; // aun no saca la potencia como n-1;
         cout<<"\n la derivada parcial de y: "<<py<<"xy^"<<nd<<endl;
+        cout<<"\n la derivada parcial de z: "<<px<<"y^"<<nd<<endl;
         
-    } else if(y!=1){
-       cout<<"hola";
+    }else if(x>1||y>1||z<1){ // || significa y 
+        px=n*x;
+        py=n*y;
+        nd=n-1;
+        z=z;
+        
+        cout<<"\n";
+        cout<<"\n la derivada parcial de x:"<<px<<"x "<<y<<"y "<<z<<"z"<<endl;
+        cout<<"\n la derivada parcial de y:"<<x<<"x "<<py<<"y^"<<nd<<" "<<z<<"z"<<endl;
+		cout<<"\n la derivada parcial de z:"<<x<<"x "<<y<<"y "<<pz<<"z"<<endl;
+    
     }
 
         
@@ -52,11 +61,42 @@ gradiente::parcial(){
     
 
 gradiente::grad(){
-    cout<<"El programa saca el gradiente:"<<endl;
-    cout<<"El gradiente es: ("<<px<<","<<py<<")"<<endl;
+    cout<<endl;
+    cout<<"*2-. Gradiente *:"<<endl;
+    
+    /*-----1-----*/
+    if(x==0,y==0,n==0,z==0){ 
+
+        cout<<endl;
+        cout<<"El gradiente es:∇: ("<<x<<","<<y<<","<<z<<")"<<endl;
+        
+    /*-----2-----*/
+    }else if(y==1) 
+    { //para no imprimir todo, tiene que imprimir ay^n
+        px= x;
+        py=px*y;
+        nd=n-1;
+        pz=0;
+        cout<<"\n";
+        cout<<"\n El gradiente es: ∇:("<<px<<","<<py<<","<<pz<<")"<<endl;
+        
+    /*-----excepcion-----*/
+    } else if (n<=0){
+        cout<<"\n La derivada no se puede resolver, por favor ingrese un N valido ";
+    } else if(z>1||y>1||z<1){ // || significa y 
+        
+        cout<<"\n El gradiente es: ("<<px<<"x "<<y<<"y "<<z<<"z "<<","<<x<<"x "<<py<<"y^"<<nd<<" "<<z<<"z "<<","<<x<<"x "<<y<<"y "<<pz<<"z "<<")"<<endl;
+        
+    }
+
 }
 
 int main(){
+
+      cout<<"______________________________________"<<endl;
+      cout<<"|Proyecto final 'Gradiente Vectorial'|"<<endl;
+      cout<<"______________________________________"<<endl;
+      cout<<"Saca la derivada parcial de la forma 'f(xyz)= Xx Yy^n Zz'\n"<<endl;
       gradiente a;
       a.parcial();
       a.grad();
