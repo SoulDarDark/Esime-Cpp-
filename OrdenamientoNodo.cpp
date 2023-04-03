bb7e2bc113554f6a96c641cca277a9c46449e4ca
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2023-04-03 12:04:35 -0600

    Update OrdenamientoNodo.cpp

--- OrdenamientoNodo.cpp
+++ OrdenamientoNodo.cpp
@@ -1,1 +1,25 @@
-void 
+void burbuja(nodo *p)
+{
+    int total=0,i,j,temp;
+    nodo *aux,*aux2;
+    aux=p;
+    while(aux!=NULL){
+        total++;
+        aux=aux->Sig;
+    }
+    cout<<" Total de datos en la lista: "<<total<<"\n";
+
+    for(i=1; i<total; i++){
+        aux=p;
+        aux2=aux->sig;
+        for(j=1;j<total;j++){
+            if(aux->dato > aux->dato){
+                temp=aux->dato; 
+                aux->dato=aux2->dato;
+                aux2->dato=temp;
+            }
+            aux=aux2;
+            aux2=aux2->sig;
+        }
+    }
+}
